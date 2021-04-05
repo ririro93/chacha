c3ac6eb6bb4b58b4c8593d3d78cf7d74cdcec736
문준하 <ririro93@gmail.com>
2021-04-05 20:02:55 +0900

    Feat: user model added to api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+
+class CustomUserView(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
--- chacha/urls.py
+++ chacha/urls.py
@@ -2,12 +2,23 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework import routers
-from questions import views
+
+from accounts.views import (
+    CustomUserView,
+)
+from questions.views import (
+    QuestionView,
+    ChoiceView,
+    AnswerView,
+)
+
+
 
 router = routers.DefaultRouter()
-router.register(r'questions', views.QuestionView, 'questions')
-router.register(r'choices', views.ChoiceView, 'choices')
-router.register(r'answers', views.AnswerView, 'answers')
+router.register(r'questions', QuestionView, 'questions')
+router.register(r'choices', ChoiceView, 'choices')
+router.register(r'answers', AnswerView, 'answers')
+router.register(r'users', CustomUserView, 'users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
