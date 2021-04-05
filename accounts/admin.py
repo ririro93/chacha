3020d32c65800c031527123920eaaf2fd706eb3d
문준하 <ririro93@gmail.com>
2021-04-05 20:54:01 +0900

    Fix: user admin page list_display

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
 
 # Register your models here.
-admin.site.register(get_user_model(), UserAdmin)
+
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    
+admin.site.register(get_user_model(), CustomUserAdmin)
