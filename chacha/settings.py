7ac2f6f0cac9f2dd9dc69b0c69aecb797f679f90
문준하 <ririro93@gmail.com>
2021-04-08 02:12:19 +0900

    Fix: login in api page

--- chacha/settings.py
+++ chacha/settings.py
@@ -161,10 +161,11 @@ ACCOUNT_UNIQUE_EMAIL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # 'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',    
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',    
+    # ],
 }
 LOGOUT_ON_PASSWORD_CHANGE = False
