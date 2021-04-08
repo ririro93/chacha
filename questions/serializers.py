edc2ba4f82d4ac280edc13cd8600e4d67589bd58
문준하 <ririro93@gmail.com>
2021-04-09 01:56:18 +0900

    Fix: questions, choices show author username instead of id

--- questions/serializers.py
+++ questions/serializers.py
@@ -2,18 +2,22 @@ from rest_framework import serializers
 from .models import Question, Choice, Answer
 
 class QuestionSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
     choices = serializers.StringRelatedField(many=True)
     class Meta:
         model = Question
         fields = ('id', 'author', 'content', 'main_question', 'created_at', 'updated_at', 'last_main_at', 'choices')
 
 class ChoiceSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
     answers = serializers.StringRelatedField(many=True)
     class Meta:
         model = Choice
         fields = ('id', 'author', 'question', 'content', 'created_at', 'answers')
 
 class AnswerSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    choice = serializers.StringRelatedField()
     class Meta:
         model = Answer
-        fields = ('id', 'choice', 'like_users', 'comment', 'created_at', 'updated_at')
+        fields = ('id', 'author', 'choice', 'like_users', 'comment', 'created_at', 'updated_at')
