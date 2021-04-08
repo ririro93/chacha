a60025ca546d30c5389fa2f141dc7794ba447f72
Eunpyeong Hong <ephong93@gmail.com>
2021-04-09 01:12:42 +0900

    Feat: display chart

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Main from '../components/Main';
 import Wall from '../components/Wall';
+import axios from 'axios';
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -13,43 +14,70 @@ class MainPage extends React.Component {
   }
 
   componentDidMount() {
+    const mainQuestion = {
+      'author': 0,
+      'content': 'Question content',
+      'created_at': Date.now(),
+      'updated_at': Date.now(),
+      'last_main': Date.now(),
+      'choices': [
+        {
+          'id': 0,
+          'content': 'Choice A',
+          'value': 30
+        },
+        {
+          'id': 1,
+          'content': 'Choice B',
+          'value': 60
+        },
+        {
+          'id': 2,
+          'content': 'Choice C',
+          'value': 10
+        }
+      ]
+    }
+    const questionList = [];
+    this.setState({
+      mainQuestion: mainQuestion,
+      questionList: questionList
+    });
+
     /*
-    axios.get('http://localhost:8000/api/questions/').then(res => {
+    axios.get('http://localhost:8000/api/questions/main').then(res => {
+      this.setState({
+        mainQuestion: res.data
+      });
+    });
+    axios.get('http://localhost:8000/api/questions/list').then(res => {
       this.setState({
         questionList: res.data
       });
     });
-    axios.get('http://localhost:8000/api/users/').then(res => {
-      this.setState({
-        userList: res.data
-      });
-    });
     */
+   
   }
 
   render() {
-    const questionList = null;
-    //const { questionList } = this.state;
     const { history } = this.props;
+    const { mainQuestion, questionList } = this.state;
 
     return (
       <div className="App">
         <Navbar history={history} ></Navbar>
         <div className="container">
-          <div className="row">
-            <Main>
-              Main!
-            </Main>
-          </div>
+          <Main mainQuestion={mainQuestion}>
+          </Main>
           <div className="row">
             <div className="col-sm">
               <Wall questionList={questionList}></Wall>
             </div>
             <div className="col-sm">
-              <Wall questionList={questionList}></Wall>
+              <Wall></Wall>
             </div>
             <div className="col-sm">
-              <Wall questionList={questionList}></Wall>
+              <Wall></Wall>
             </div>
           </div>
         </div>
