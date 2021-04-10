16ba69f35ac29771b889b89484b304a650938abc
문준하 <ririro93@gmail.com>
2021-04-10 11:48:51 +0900

    Docs: README edit

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # chacha
+> Django REST Framework(backend) + React(frontend) <br>
+트위터가 글 중심, 인스타가 사진 중심 커뮤니티라면 이 앱은 그래프 중심 커뮤니티랄까
 
-## 배운 점 
+## Goals
 - Django REST Framework
-- Session auth vs JWT(Json Web Token)
+- JWT(Json Web Token) auth
+
+## Todos
+- [x]  로그인 기능, 회원가입 기능
+    - [x] /accounts/login/
+    - [x] /accounts/register/
+- [x] Question 생성 페이지 만들기
+- [ ] url 싹 다 정리하기
+- [ ] 필요한 api
+    - [ ] get current user info→ profile
+    - [x] get is-logged-in → 필요 없을듯
+    - [x] post question
+- [x] main question 보낼 때
+    - [x] answer 수만 리스트로 보내기
+    - [x] answer 정보는 보내지 말기 (브라우저로 볼 수 있으니깐)
+- [x] choices 는 기본적으로 question 등록할 때 같이 할 수 있게 → modelviewset customizing 할 수 있으면 json으로 choices 목록 오는거 views에서 등록하기
+- [ ] rest auth 말고 그냥 기본 rest framework jwt 써보기
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -22,6 +22,31 @@ class QuestionView(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        """
+        create 할 때 choice까지 같이 넣도록 customizing 해주기
+        """
+        print('## request.data', request.data)
+
+        # create new question
+        choices = request.data.pop('choices')
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        # create new choices
+        question = Question.objects.get(pk=serializer.data.get('id'))
+        for choice in choices:
+            Choice.objects.create(
+                content=choice,
+                question=question,
+            )
+        
+        # return data
+        new_serializer = QuestionSerializer(question)
+        return Response(new_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @action(detail=False, url_path='main-question')
     def main_question(self, request, *args, **kwargs):
